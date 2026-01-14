84431de159e3b0d747d2901b4eb52c42e36cca23
isaacli <isaacli@andrew.cmu.edu>
2026-01-13 20:53:24 -0500

    Completed copying over skeleton.

--- work/server.c
+++ work/server.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <err.h>
 
+#include <stdbool.h>
+
 #define MAXMSGLEN 100 
 
 // Recall: argc is number or arguments, argv is the array of char* strings. 
@@ -29,5 +31,54 @@ int main(int argc, char** argv) {
     }
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // TCP/IP
+    if (sockfd < 0) {
+        err(1, 0); // Prints an error based on errorno and exits the program. 
+    }
+
+    // setup address structure to indicate server port
+	memset(&srv, 0, sizeof(srv));			// clear it first
+	srv.sin_family = AF_INET;			// IP family
+	srv.sin_addr.s_addr = htonl(INADDR_ANY);	// don't care IP address
+	srv.sin_port = htons(port);			// server port
+
+	// bind to our port
+	rv = bind(sockfd, (struct sockaddr*)&srv, sizeof(struct sockaddr));
+	if (rv<0) err(1,0);
+	
+	// start listening for connections
+	rv = listen(sockfd, 5);
+    if (rv < 0) {
+        err(1, 0);
+    }
+
+    // Main server loop, set up to go indefinitely one at a time. 
+    while (true) {
+
+        // Wait for next client, get session socket. 
+        sa_size = sizeof(struct sockaddr_in); 
+        sessfd = accept(sockfd, (struct sockaddr *)&cli, &sa_size);
+        if (sessfd < 0) {
+            err(1, 0); 
+        }
+
+        // Get messages and echo them until done. 
+
+        // Loops while we still are able to read bytes into buf. 
+        while ( (rv = recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
+            buf[rv] = 0; // Adds a null character to make string inputs.  
+        }
+        if (rv < 0) {
+            err(1, 0);
+            close(sessfd);
+        } 
+    }
+
+    close(sockfd);
+
+
+
+    return 0;
+
+
 
 }
